99b51af170703b9e0ef3908c4f358ac3cfda6902
Zarathu5trA <prizrak-keks@mail.ru>
2014-11-13 14:14:34 +0100

    UART and TERM

---
+++ aos_env_pub.sh
@@ -0,0 +1,6 @@
+AOS_PATH=/pub/aos
+ARM_GCC=$AOS_PATH/tools13/arm-2012.03/bin
+GHC=$AOS_PATH/tools13/ghc-7.4.1-pub/ghc-7.4.1/bin
+export PATH=$ARM_GCC:$GHC:$AOS_PATH/tools13/bin:$PATH
+
+export AOS_PATH
---
+++ m2/usr/init/ipc_protocol.h
@@ -0,0 +1,18 @@
+#ifndef IPC_PROTOCOL_H
+    #define IPC_PROTOCOL_H
+    
+    #define AOS_REGISTER_SERVICE        0x00000100
+    #define AOS_REGISTRATION_COMPETE    0x00000101
+    #define AOS_GET_SERVICE             0x00000102
+    
+    #define UART_CONNECT                0x00000200
+    #define UART_DISCONNECT             0x00000201
+    #define UART_RECV_BYTE              0x00000202
+    #define UART_SEND_BYTE              0x00000203
+    
+
+    #define SERVICE_UART_DRIVER         0x00000100
+        
+    #define STATUS_SUCCESS              0x00000000
+    #define STATUS_ALREADY_EXIST        0x00000001    
+#endif
