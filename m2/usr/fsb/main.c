4980826cf40926d18eedacd9594011869746716a
Zarathu5trA <prizrak-keks@mail.ru>
2014-12-24 17:27:01 +0100

    FSB enhancement

--- m2/usr/fsb/main.c
+++ m2/usr/fsb/main.c
@@ -8,7 +8,7 @@
 #include <barrelfish/lmp_chan.h>
 #include <omap_timer/timer.h>
 
-#define BUFSIZE (48UL*1024*1024)
+#define BUFSIZE (1UL*1024*1024)
 
 static struct aos_rpc* filesystem_channel;
 
@@ -27,9 +27,10 @@ static errval_t evaluate_fs(const int file)
 {
     char    * chunk     =       NULL;
     uint32_t  chunk_id  =          0;
-    size_t    chunk_len =         27;
+    size_t    chunk_len =    BUFSIZE;
     uint64_t  elapsed  ;
     errval_t  error     = SYS_ERR_OK;
+    uint32_t  size      =          0;
     uint64_t  start    ;
 
     omap_timer_init(    );
@@ -38,9 +39,11 @@ static errval_t evaluate_fs(const int file)
     start = omap_timer_read();
 
     // START
-    for (; (chunk_len == 27) && err_is_ok(error) ; chunk_id++){
-        error = aos_rpc_read(filesystem_channel, file, chunk_id * 27, 27, (void**)&chunk, &chunk_len);
+    for (; (chunk_len == BUFSIZE) && err_is_ok(error) ; chunk_id++){
+        debug_printf("ping\n");
+        error = aos_rpc_read(filesystem_channel, file, chunk_id * BUFSIZE, BUFSIZE, (void**)&chunk, &chunk_len);
         if (err_is_ok(error)) {
+            size += chunk_len;
             free(chunk);
         }
     }
@@ -50,40 +53,45 @@ static errval_t evaluate_fs(const int file)
 
     if (err_is_ok(error)) {
         printf ("Elapsed time: %llu\n", elapsed);
+        printf ("File size   : %u\n"  , size   );
     } else {
         printf ("Error during reading!\n");    
-    } 
+    }
 
     return error;
 }
 
 int main(int argc, char *argv[])
 {
-    errval_t error = -1;
+    if (argc != 2) {
+        printf("There is an argument required - path to the test file\n");
+    } else {
+        errval_t error = -1;
 
-    printf ("Filesystem benchmark: %s\n", argv[0]);
+        printf ("Filesystem benchmark: %s\n", argv[1]);
 
-    filesystem_channel = malloc (sizeof (struct aos_rpc));
-    if (filesystem_channel) {
-        struct capref fs_cap;
+        filesystem_channel = malloc (sizeof (struct aos_rpc));
+        if (filesystem_channel) {
+            struct capref fs_cap;
 
-        error = aos_find_service (aos_service_filesystem, &fs_cap);
-        print_error (error, "fsbench: aos_find_service failed. %s\n", err_getstring (error));
-        if (err_is_ok(error)) {
-            error = aos_rpc_init (filesystem_channel, fs_cap);
-            print_error (error, "fsbench: aos_rpc_init failed. %s\n", err_getstring (error));
-            if (err_is_ok(error)) { 
-                error = aos_ping (filesystem_channel, 42); // TODO: ????
-                print_error (error, "fsbench: aos_ping failed. %s\n", err_getstring (error));
-                if (err_is_ok(error)) {
-                    int      file ;
-
-                    error = aos_rpc_open(filesystem_channel, "/bench/data.txt", &file);
-                    print_error (error, "fsbench: aos_rpc_open failed. %s\n", err_getstring (error));
+            error = aos_find_service (aos_service_filesystem, &fs_cap);
+            print_error (error, "fsbench: aos_find_service failed. %s\n", err_getstring (error));
+            if (err_is_ok(error)) {
+                error = aos_rpc_init (filesystem_channel, fs_cap);
+                print_error (error, "fsbench: aos_rpc_init failed. %s\n", err_getstring (error));
+                if (err_is_ok(error)) { 
+                    error = aos_ping (filesystem_channel, 42);
+                    print_error (error, "fsbench: aos_ping failed. %s\n", err_getstring (error));
                     if (err_is_ok(error)) {
-                        error = evaluate_fs(file);
+                        int      file ;
+
+                        error = aos_rpc_open(filesystem_channel, argv[1], &file);
+                        print_error (error, "fsbench: aos_rpc_open failed. %s\n", err_getstring (error));
+                        if (err_is_ok(error)) {
+                            error = evaluate_fs(file);
                         
-                        aos_rpc_close(filesystem_channel, file);
+                            aos_rpc_close(filesystem_channel, file);
+                        }
                     }
                 }
             }
@@ -94,4 +102,3 @@ int main(int argc, char *argv[])
 
     return 0;
 }
-
